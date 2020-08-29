6c141d72b20cdd7c5745328d4a866cad0071363e
VITGEARSHCS <aashish.bansal2019@vitstudent.ac.in>
2020-08-29 16:22:30 +0530

    Next Questions

---
+++ ITE1002 PC Web Technologies Laboratory/Assessment 2/19BIT0346 Question 4.js
@@ -0,0 +1,16 @@
+var x = document.getElementById("phrase").value;
+document.write(x);
+/*
+function reverse(){
+    //var n, x, i, j;
+    var string=phrase;
+    rev = "";
+    //n = window.prompt("enter any string");
+    x = string.split(" ");
+    for (i = 0; i < x.length; i++) {
+        for (j = x[i].length() - 1; j >= 0; j--) {
+            document.writeln(x[i].charAt(j));
+            document.write(rev);
+        }
+    }
+}*/
