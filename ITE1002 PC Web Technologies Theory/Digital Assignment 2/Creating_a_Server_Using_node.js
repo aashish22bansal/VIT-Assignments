353db6814ce9267659f90f03675be8d5fa31365b
Aashish Bansal <aashish22bansal@gmail.com>
2020-10-04 19:17:50 +0530

    Opening a File Using Node.js

--- ITE1002 PC Web Technologies Theory/Digital Assignment 2/Creating_a_Server_Using_node.js
+++ ITE1002 PC Web Technologies Theory/Digital Assignment 2/Creating_a_Server_Using_node.js
@@ -1,5 +1,13 @@
 var http = require('http');
+var fs = require("fs");
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hello! Your Node.js Server is ready');
+    var path = "C:\\xampp\\htdocs\\VIT-Assignments\\ITE1002 PC Web Technologies Theory\\Digital Assignment 2\\Home_Page.html";
+
+    fs.open(path, "w+", function(error, fd) {
+    if (error) {
+        console.error("open error:  " + error.message);
+    } else {
+        console.log("Successfully opened " + path);
+    }
+    });
 }).listen(3000);
