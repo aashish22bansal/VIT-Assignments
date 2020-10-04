4f60dc541030d748f793d1e6667c46d0f6e4e8d3
Aashish Bansal <aashish22bansal@gmail.com>
2020-10-04 19:22:02 +0530

    NOT RUNNING

--- ITE1002 PC Web Technologies Theory/Digital Assignment 2/Creating_a_Server_Using_node.js
+++ ITE1002 PC Web Technologies Theory/Digital Assignment 2/Creating_a_Server_Using_node.js
@@ -4,10 +4,10 @@ http.createServer(function(req,res){
     var path = "C:\\xampp\\htdocs\\VIT-Assignments\\ITE1002 PC Web Technologies Theory\\Digital Assignment 2\\Home_Page.html";
 
     fs.open(path, "w+", function(error, fd) {
-    if (error) {
-        console.error("open error:  " + error.message);
-    } else {
-        console.log("Successfully opened " + path);
-    }
+        if (error) {
+            console.error("open error:  " + error.message);
+        } else {
+            console.log("Successfully opened " + path);
+        }
     });
 }).listen(3000);
