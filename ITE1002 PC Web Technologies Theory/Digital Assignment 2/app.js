78e89b46d1f694d68c67c7d9bd5f73c9d5f35193
Aashish Bansal <aashish22bansal@gmail.com>
2020-10-05 08:16:17 +0530

    Theory

---
+++ ITE1002 PC Web Technologies Theory/Digital Assignment 2/app.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require("fs");
+http.createServer(function(req,res){
+    var path = "C:\\Users\\aashi\\Documents\\GitHub Desktop\\VIT-Assignments\\ITE1002 PC Web Technologies Theory\\Digital Assignment 2\\Home_Page.html";
+
+    fs.open(path, "w+", function(error, fd) {
+        if (error) {
+            console.error("open error:  " + error.message);
+        } else {
+            console.log("Successfully opened " + path);
+        }
+    });
+}).listen(3000);
