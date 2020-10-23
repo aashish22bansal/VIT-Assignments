2d89c95c5078152c4e8c58c298c0e8b571ecebd8
Aashish Bansal <aashish22bansal@gmail.com>
2020-10-23 13:43:41 +0530

    Question 7

--- ITE1002 PC Web Technologies Laboratory/Assessment 5/Question_7/19BIT0346_Question_7_Part_1.js
+++ ITE1002 PC Web Technologies Laboratory/Assessment 5/Question_7/19BIT0346_Question_7_Part_1.js
@@ -7,18 +7,20 @@ MongoClient.connect(url, function(err, db) {
 		throw err;
 	}
 	var dbo = db.db("company_db");
-	var Employee1 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
-	var Employee2 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
-	var Employee3 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
-	var Employee4 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
-	var Employee5 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
-	var Employee6 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
-	var Employee7 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
-	var Employee8 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
-	var Employee9 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
-	var Employee10 = { EmpID:"", Vehicle_No: "", Owner_Name: "", Brand_Name:"", Year_Of_Purchase="" };
+	var Employee1 = { EmpID:"1", Vehicle_No: "KA 01 AA 0001", Owner_Name: "A", Brand_Name:"Ford", Year_Of_Purchase:"2020" };
+	var Employee2 = { EmpID:"2", Vehicle_No: "KA 01 AA 0002", Owner_Name: "B", Brand_Name:"Toyota", Year_Of_Purchase:"2019" };
+	var Employee3 = { EmpID:"3", Vehicle_No: "KA 01 AA 0003", Owner_Name: "C", Brand_Name:"Mercedes", Year_Of_Purchase:"2018" };
+	var Employee4 = { EmpID:"4", Vehicle_No: "KA 01 AA 0004", Owner_Name: "D", Brand_Name:"BMW", Year_Of_Purchase:"2017" };
+	var Employee5 = { EmpID:"5", Vehicle_No: "KA 01 AA 0005", Owner_Name: "E", Brand_Name:"Lamborgini", Year_Of_Purchase:"2016" };
+	var Employee6 = { EmpID:"6", Vehicle_No: "KA 01 AA 0006", Owner_Name: "F", Brand_Name:"Hyundai", Year_Of_Purchase:"2015" };
+	var Employee7 = { EmpID:"7", Vehicle_No: "KA 01 AA 0007", Owner_Name: "G", Brand_Name:"Mahindra", Year_Of_Purchase:"2014" };
+	var Employee8 = { EmpID:"8", Vehicle_No: "KA 01 AA 0008", Owner_Name: "H", Brand_Name:"Maruti", Year_Of_Purchase:"2013" };
+	var Employee9 = { EmpID:"9", Vehicle_No: "KA 01 AA 0009", Owner_Name: "I", Brand_Name:"Volvo", Year_Of_Purchase:"2012" };
+	var Employee10 = { EmpID:"10", Vehicle_No: "KA 01 AA 0010", Owner_Name: "J", Brand_Name:"Nissan", Year_Of_Purchase:"2011" };
 	dbo.collection("Employee_Details_Collection").insertMany([Employee1,Employee2,Employee3,Employee4,Employee5,Employee6,Employee7,Employee8,Employee9,Employee10], function(err, res) {
-		if (err) throw err;
+		if (err){
+			throw err;
+		}
 		console.log("10 documents inserted");
 		db.close();  
 	});
