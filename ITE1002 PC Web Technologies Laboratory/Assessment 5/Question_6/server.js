6bfbfae427299787bd5f09565bf3b58c31cb2395
Aashish Bansal <aashish22bansal@gmail.com>
2020-10-25 16:22:40 +0530

    Question 7 Completed

--- ITE1002 PC Web Technologies Laboratory/Assessment 5/Question_6/server.js
+++ ITE1002 PC Web Technologies Laboratory/Assessment 5/Question_6/server.js
@@ -7,6 +7,90 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const app =	express();
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+MongoClient.connect(url, function(err,db){
+	if(err){
+		throw err;
+	}
+	
+	app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+	app.use(bodyParser.json());      
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(express.static(__dirname + '/views'));
+
+	var sess; // global session, NOT recommended
+
+	router.get('/',(req,res) => {
+		sess = req.session;
+		if(sess.email) {
+			return res.redirect('/admin');
+		}
+		res.sendFile('index.html');
+	});
+
+	router.post('/login',(req,res) => {
+		sess = req.session;
+		sess.email = req.body.email;
+		res.end('done');
+	});
+
+	router.get('/admin',(req,res) => {
+		sess = req.session;
+		var dbo = db.db("Question6_db");
+		var query = {username: sess.email, password: sess.password};
+		console.log(query);
+		if(dbo.collection("Question_6_Collection").find(query).toArray(function(err,result){
+			if(err){
+				throw err;
+			}
+			console.log(result);
+			return true;
+		}))
+		{
+			if(sess.email) {
+				res.write(`<h1>Hello ${sess.email} </h1><br>`);
+				res.end('<a href='+'/logout'+'>Logout</a>');
+			}
+			else {
+				res.write('<h1>Please login first.</h1>');
+				res.end('<a href='+'/'+'>Login</a>');
+			}
+		}
+		else
+		{
+			res.write('Credential Not found!\nUse Valid Credentaials.');
+		}
+	});
+
+	router.get('/logout',(req,res) => {
+		req.session.destroy((err) => {
+			if(err) {
+				return console.log(err);
+			}
+			res.redirect('/');
+		});
+
+	});
+
+	app.use('/', router);
+	/*
+	var dbo = db.db("Question6_db");
+	var query = {username: sess.email, password: sess.password};
+	if(dbo.collection("Question_6_Collection").find(query))
+	{
+		if(sess.email) {
+			res.write(`<h1>Hello ${sess.email} </h1><br>`);
+			res.end('<a href='+'/logout'+'>Logout</a>');
+		}
+		else {
+			res.write('<h1>Please login first.</h1>');
+			res.end('<a href='+'/'+'>Login</a>');
+		}
+	}
+	*/
+});
+/*
 app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
 app.use(bodyParser.json());      
 app.use(bodyParser.urlencoded({extended: true}));
@@ -51,7 +135,7 @@ router.get('/logout',(req,res) => {
 });
 
 app.use('/', router);
-
+*/
 app.listen(process.env.PORT || 3000,() => {
 	console.log(`App Started on PORT ${process.env.PORT || 3000}`);
 });
--- ITE1002 PC Web Technologies Laboratory/Assessment 5/Question_7/19BIT0346_Question_7_Part_2_Adding_Handler_for_Data_View.js
+++ ITE1002 PC Web Technologies Laboratory/Assessment 5/Question_7/19BIT0346_Question_7_Part_2_Adding_Handler_for_Data_View.js
@@ -2,61 +2,57 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
-
-//const url = "mongodb://localhost:27017";
-//var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017/company_db');
-const client = mongodb.MongoClient.connect('mongodb://localhost:27017',{ useNewUrlParser: true , useUnifiedTopology : true});
-
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/', function(err, db){	  
+    if (err){
+        throw err;
+    }
+	console.log("Connected to Database");
+	dbo=db.db("company_db");
+});
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.get('/index', function (req, res) {
+   res.sendFile( __dirname + "/public/" + "index.html" );})
+   
 app.post('/post-feedback', function (req, res) {
-    client.then(function(db) {
-        delete req.body._id; // for safety reasons
-        db.collection('Employee_Details_Collection').insertOne(req.body);
-    });    
-    res.send('Data received:\n' + JSON.stringify(req.body));
-});
-
-app.post('/view-feedbacks',  function(req, res) {
-    client.then(function(db) {
-        db.collection('Employee_Details_Collection').find({}).toArray().then(function(feedbacks) {
-            res.status(200).json(feedbacks);
-        });
-    });
-});
-
-app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
-/*
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var mongodb = require('mongodb');
-
-var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017',{ useNewUrlParser: true , useUnifiedTopology : true});
-
-var app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.resolve(__dirname, 'public')));
-
-app.post('/post-feedback', function (req, res) {
-    dbConn.then(function(db) {
-        delete req.body._id; // for safety reasons
-        db.collection('Employee_Details_Collection').insertOne(req.body);
-    });    
-    res.send('Data received:\n' + JSON.stringify(req.body));
-});
+    var oldQuery={EmpID:req.body.old_employee_id,
+        Vehicle_No:req.body.old_vehicle_no,
+		Owner_Name:req.body.old_owner_name,
+		Brand_Name:req.body.old_brand_name,
+        Year_Of_Purchase:req.body.old_year_of_purchase};
+        
+	var response={$set:{EmpID:req.body.new_employee_id,
+		Vehicle_No:req.body.new_vehicle_no,
+		Owner_Name:req.body.new_owner_name,
+		Brand_Name:req.body.new_brand_name,
+		Year_Of_Purchase:req.body.new_year_of_purchase}};
+       
+		
+		//insert document in mongodb
+		dbo.collection('Employee_Details_Collection').updateOne(oldQuery, response, function(err, result) 	
+			{	
+                if (err){
+                    throw err;
+                }
+				console.log("1 document updated in your mongodb database" ); 
+				
+			});
+		
+		console.log(response); // display in node console window
+	res.end(JSON.stringify(response));}) // display in browser window
+  
 
 app.get('/view-feedbacks',  function(req, res) {
-    dbConn.then(function(db) {
-        db.collection('Employee_Details_Collection').find({}).toArray().then(function(feedbacks) {
+    
+        dbo.collection('Employee_Details_Collection').find({}).toArray().then(function(feedbacks) {
             res.status(200).json(feedbacks);
         });
-    });
+    
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );*/
+app.listen(3000,function(){console.log("Server started")});
