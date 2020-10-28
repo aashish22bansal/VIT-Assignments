612ace46c9600c3aab72e76f46c6361549ba1ed7
Aashish Bansal <aashish22bansal@gmail.com>
2020-10-28 16:42:55 +0530

    different approach

--- ITE1002 PC Web Technologies Laboratory/Assessment 5/Question_2/public/js/script.js
+++ ITE1002 PC Web Technologies Laboratory/Assessment 5/Question_2/public/js/script.js
@@ -13,28 +13,5 @@ var app = angular.module('myApp',['smart-table']);
                 document.getElementById("scoreA").nodeValue = score_valA;
             }
 
-            // Create an eventEmitter object
-            var scoreKeeper = new events.EventEmitter();
-
-            // Create an event Handler function
-            var shoot_a_basket = function connected(){
-                console.log('Connection Successful!');
-
-                // Fire up the data_received event
-                scoreKeeper.emit('data_received');
-            }
-
-            // Binding the connection event with the handler
-            scoreKeeper.on('connection',shoot_a_basket);
-
-            // Bind the data_received event with the anonymous function
-            scoreKeeper.on('data_received',function(){
-                document.getElementById("1_pointerA").innerHTML(document.getElementById("1_pointerA").addEventListener("click",add_1PointerA));
-                console.log('data received successfully.');
-            });
-
-            // Fire up the connection event
-            scoreKeeper.emit('connection');
-
-            console.log("Program Ended.");
+            
         }]);
